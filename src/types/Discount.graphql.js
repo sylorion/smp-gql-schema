71ba1d101a5fbcc9cf5e3018f7e4a8b876c418fb
J. Charles N. M <jcharles.nmbiada@gmail.com>
2023-12-16 15:52:02 +0100

    Sync Discount with it table creation update

--- src/types/Discount.graphql.js
+++ src/types/Discount.graphql.js
@@ -2,39 +2,44 @@ export default `
 # src/graphql/types/Discount.graphql.js
 
 type Discount implements ServicesEntity & ServicesNavigableEntity & ServicesStatable {
-  discountID: ID!
-  uniqRef: String
-  slug: String
-  authorID: ID
-  description: String
-  discountCode: String
-  discountValue: Int
-  discountInPercent: Boolean!
-  startDate: DateTime
-  endDate: DateTime
-  serviceID: ID
-  organizationID: ID
-  topicID: ID
-  state: ObjectStatus
-  createdAt: DateTime
-  updatedAt: DateTime
-  deletedAt: DateTime
+  discountID :: ID!
+  uniqRef :   String!
+  slug :  String!
+  authorID :  ID
+  description :   String
+  discountCode :  String
+  discountValue :   Int
+  discountInPercent :   Boolean!
+  startDate :   DateTime
+  endDate :   DateTime
+  organizationID :  ID
+  topicID :   ID
+  serviceID :   ID
+  userID :  ID
+  industryID :  ID
+  tagIDs :   [ID]
+  state :   ObjectStatus
+  createdAt :   DateTime
+  updatedAt :   DateTime
+  deletedAt :   DateTime
 }
 
 input DiscountInput {
-  discountID: ID
-  authorID: ID
-  description: String
-  discountCode: String
-  discountValue: Int
-  discountInPercent: Boolean
-  startDate: DateTime
-  endDate: DateTime
-  serviceID: ID
-  organizationID: ID
-  topicID: ID
-  state: ObjectStatus
-} 
+  discountID :: ID
+  authorID :  ID
+  description :   String
+  discountCode :  String
+  discountValue :   Int
+  discountInPercent :   Boolean!
+  startDate :   DateTime
+  endDate :   DateTime
+  organizationID :  ID
+  topicID :   ID
+  serviceID :   ID
+  userID :  ID
+  industryID :  ID
+  tagIDs :   [ID]
+  state :   ObjectStatus}
 
 extend type Query {
   discount(discountID: ID!): Discount
