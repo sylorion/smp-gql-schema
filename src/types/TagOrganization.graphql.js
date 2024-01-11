68b23a8f3b773a1fde22369fafb5150d56c2eaa3
zaengetsu <106625404+zaengetsu@users.noreply.github.com>
2024-01-12 00:51:35 +0100

    ajout du type TagOrganization

---
+++ src/types/TagOrganization.graphql.js
@@ -0,0 +1,41 @@
+export default `
+# src/graphql/types/TagOrganization.graphql.js
+
+type TagOrganization implements ServicesEntity & ServicesNavigableEntity & ServicesStatable {
+  tagOrganizationID: ID!
+  tagID: ID
+  organizationID: ID
+  state: ObjectStatus
+  createdAt: DateTime
+  updatedAt: DateTime
+  deletedAt: DateTime
+}
+
+input TagOrganizationInput {
+  tagOrganizationID: ID
+  tagID: ID!
+  organizationID: ID!
+  state: ObjectStatus
+}
+
+extend type Query {
+  tagOrganization(tagOrganizationID: ID!): TagOrganization
+  tagOrganizations(
+    pagination: PaginationInput,
+    sort: SortInput,
+    filter: [FilterInput!]
+  ): [TagOrganization!]!
+}
+
+type Mutation {
+  createTagOrganization(input: TagOrganizationInput!): TagOrganization!
+  updateTagOrganization(tagOrganizationID: ID!, input: TagOrganizationInput!): TagOrganization!
+  deleteTagOrganization(tagOrganizationID: ID!): MutationResponse!
+}
+
+type Subscription {
+  tagOrganizationAdded: TagOrganization!
+  tagOrganizationUpdated: TagOrganization!
+  tagOrganizationDeleted: TagOrganization!
+}
+`;
