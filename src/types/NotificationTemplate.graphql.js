b31ac39c00a00a05b12655086302432e22f77a36
zaengetsu <106625404+zaengetsu@users.noreply.github.com>
2024-03-26 10:41:27 +0100

    ajout du shchema de NotificationTemplate

---
+++ src/types/NotificationTemplate.graphql.js
@@ -0,0 +1,41 @@
+export default `
+# src/graphql/types/NotificationTemplate.graphql.js
+
+type NotificationTemplate {
+  notificationTemplateID: ID!
+  title: String!
+  message: String!
+  readAt: DateTime
+  createdAt: DateTime
+  updatedAt: DateTime
+  deletedAt: DateTime
+}
+
+input NotificationTemplateInput {
+  name: String!
+  title: String!
+  message: String!
+}
+
+extend type Query {
+  notificationTemplate(notificationTemplateID: ID!): NotificationTemplate
+  notificationTemplates(
+    pagination: PaginationInput,
+    sort: SortInput,
+    filter: [FilterInput!]
+  ): [NotificationTemplate!]!
+}
+
+type Mutation {
+  createNotificationTemplate(input: NotificationTemplateInput!): NotificationTemplate!
+  updateNotificationTemplate(notificationTemplateID: ID!, input: NotificationTemplateInput!): NotificationTemplate!
+  deleteNotificationTemplate(notificationTemplateID: ID!): MutationResponse!
+  markNotificationTemplateAsRead(notificationTemplateID: ID!): NotificationTemplate!
+}
+
+type Subscription {
+  notificationTemplateAdded: NotificationTemplate!
+  notificationTemplateUpdated: NotificationTemplate!
+  notificationTemplateDeleted: NotificationTemplate!
+}
+`;
