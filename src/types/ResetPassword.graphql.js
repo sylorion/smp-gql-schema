89e3f6e14229a4bb19dd5c43b13c510040adef47
zaengetsu <106625404+zaengetsu@users.noreply.github.com>
2024-06-06 07:13:49 +0200

    sppression de l'obligationd e retourner un succes pour debug

--- src/types/ResetPassword.graphql.js
+++ src/types/ResetPassword.graphql.js
@@ -2,19 +2,14 @@ export default `
 # src/graphql/types/ResetPassword.graphql.js
 
  type Mutation {
-  # Mutation pour demander la réinitialisation de mot de passe
-
   requestPasswordReset(email: String!): PasswordResetResponse!
-
-  # Mutation pour réinitialiser le mot de passe avec le token reçu par email
-
   resetPassword(token: String!, newPassword: String!): PasswordResetResponse!
 } 
 
 # Réponse des mutations de réinitialisation de mot de passe
 
 type PasswordResetResponse {
-  success: Boolean!
+  success: Boolean
   message: String
 }
 `;
