f60be830d9af47152debcc164af88fff7261c27e
zaengetsu <106625404+zaengetsu@users.noreply.github.com>
2024-06-09 13:38:09 +0200

    correction de l'utilisation des inputs

--- src/types/ResetPassword.graphql.js
+++ src/types/ResetPassword.graphql.js
@@ -1,11 +1,6 @@
 export default `
 # src/graphql/types/ResetPassword.graphql.js
 
- type Mutation {
-  forgotPassword(email: String!): PasswordResetResponse!
-  resetPassword(token: String!, newPassword: String!): PasswordResetResponse!
-} 
-
 input ForgotPasswordInput {
   email: String!
 }
@@ -14,10 +9,15 @@ input ResetPasswordInput {
   token: String!
   newPassword: String!
 }
-# Réponse des mutations de réinitialisation de mot de passe
 
 type PasswordResetResponse {
   success: Boolean
   message: String
 }
+
+type Mutation {
+  forgotPassword(input: ForgotPasswordInput!): PasswordResetResponse!
+  resetPassword(input: ResetPasswordInput!): PasswordResetResponse!
+}
+
 `;
