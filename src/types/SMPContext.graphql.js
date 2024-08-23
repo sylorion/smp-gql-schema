08cf351ed7369c9087ae967b6dcaf912622d6e1a
J. Charles N. M <jcharles.nmbiada@gmail.com>
2024-08-23 17:27:55 +0200

    Deleting Heartbeat type in SMPContext.graphql.js

--- src/types/SMPContext.graphql.js
+++ src/types/SMPContext.graphql.js
@@ -1,15 +1,6 @@
 
 export default `
 # src/graphql/types/SMPContext.graphql.js
-type Heartbeat {
-  lastPing: Date
-  interval: Int
-  timeOut: Int
-  timeOutCount: Int
-  heartbeat: Int
-  maxTimeOut: Int
-  }
-
 type Query {
   hello: String
   version: String
