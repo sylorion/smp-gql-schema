b6c21ab156bcaf545b04be4a73430db97dd093de
J. Charles N. M <jcnm@sylorion.com>
2024-10-01 18:48:02 +0200

    Update Applications and ApplicationToken schemas

--- src/types/ApplicationToken.graphql.js
+++ src/types/ApplicationToken.graphql.js
@@ -14,10 +14,37 @@ type ApplicationToken {
   deletedAt: DateTime
 }
 
-input CreateApplicationTokenInput {
+type ApplicationAccessTokenRefreshResponse {
+  accessToken: String
+  expiresIn: Int
+  message: String
+  errors: [MutationError!]
+}
+
+type ApplicationDetails {
   applicationID: ID!
-  key: String!
-  isActive: Boolean!
+  uniqRef: String
+  slug: String
+  title: String
+  description: String
+  officialName: String
+  developerID: ID
+  authKey: String
+  plan: String
+  appConfiguration: JSON
+  state: ObjectStatus
+  createdAt: DateTime!
+  updatedAt: DateTime 
+}
+
+type ApplicationLoginResponse {
+  accessToken: String!
+  refreshToken: String!
+  accessValidityDuration: Int!
+  refreshValidityDuration: Int!
+  application: ApplicationDetails
+  message: String
+  errors: [MutationError!]
 }
 
 input UpdateApplicationTokenInput {
@@ -31,11 +58,12 @@ extend type Query {
   applicationTokensByIDs(applicationTokenIDs: [ID!]!): [ApplicationToken!]!,
   applicationTokensBySlugs(slugs: [String!]!): [ApplicationToken!]!
   applicationTokenByUniqRef(UniqRef: String!): ApplicationToken!
-
 }
 
 type Mutation {
-  createApplicationToken(input: CreateApplicationTokenInput!): ApplicationToken!
+  createApplicationToken(applicationID: ID, applicationKey: String!): ApplicationLoginResponse!
+  refreshApplicationAccessToken(refreshToken: String!): ApplicationAccessTokenRefreshResponse
+  applicationSignIn(applicationID: ID, applicationKey: String!): ApplicationLoginResponse!
   updateApplicationToken(applicationTokenID: ID!, input: UpdateApplicationTokenInput!): ApplicationToken!
   deleteApplicationToken(applicationTokenID: ID!): MutationResponse!
 }
