ae33aaab4c294f3b3f44712e9dc03b5604ea879b
J. Charles N. M <jcnm@sylorion.com>
2024-10-06 23:26:02 +0200

    Add directive @exclude @secure and @transform

--- src/types/BaseType.graphql.js
+++ src/types/BaseType.graphql.js
@@ -10,13 +10,27 @@ scalar Date
 interface ServicesEntity {
   uniqRef: String # Mostly hand generated ID except for mongodb data
 }
-
 interface ServicesNavigableEntity {
   slug: String
 }
-
 interface ServicesStatable {
   state: ObjectStatus
 }
+directive @exclude on 
+FIELD_DEFINITION | INPUT_FIELD_DEFINITION 
+| UNION | INTERFACE | ENUM | OBJECT
+
+directive @secure(hash: string, comment: string) on 
+FIELD_DEFINITION | INPUT_FIELD_DEFINITION | UNION | INTERFACE | ENUM_VALUE 
+
+directive @transform(
+  type: String!
+  name: String
+  custom_type: String
+  repeated: Boolean
+  oneof: String
+  map_key: String
+  map_value: String
+) on FIELD_DEFINITION | INPUT_FIELD_DEFINITION | ARGUMENT_DEFINITION | ENUM_VALUE | SCALAR | UNION | INTERFACE | ENUM | INPUT_OBJECT | OBJECT
 
 `
